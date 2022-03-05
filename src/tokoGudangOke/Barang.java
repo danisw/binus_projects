e87e2070dba6306e97d41500dba5e8d89b1963b6
mita <mitadaniswari@gmail.com>
2022-03-05 13:06:39 +0700

    finishing_look

--- src/tokoGudangOke/Barang.java
+++ src/tokoGudangOke/Barang.java
@@ -20,34 +20,36 @@ public class Barang {
         nama_user = key.nextLine();
 
         //Welcome Message
-        System.out.println("Hello ini Toko Gudang Oke!\n");
-        System.out.println("Stock Gudang Input Barang\n");
-        System.out.println("#########################\n");
+        System.out.println("Stock Gudang Input Barang");
+        System.out.println("#########################");
 
-        System.out.println("Selamat Datang " + nama_user + " !");
+        System.out.println("Selamat Datang oko Gudang Oke, " + nama_user + " !");
 
         //memasukkan nama barang
         System.out.print("Masukkan nama barang yang akan ditambah : ");
         nama_barang = key.nextLine();
 
         //memasukkan jumlah barang
-        System.out.print("\nMasukkan jumlah barang yang akan ditambah : ");
+        System.out.print("Masukkan jumlah barang yang akan ditambah : ");
         jumlah_barang = key.nextInt();
 
         //memasukkan harga beli barang
-        System.out.print("\nMasukkan harga beli barang tersebut : ");
+        System.out.print("Masukkan harga beli barang tersebut : ");
         harga_beli = key.nextDouble();
 
         //memasukkan harga jual nbarang
-        System.out.print("\nMasukkan harga jual barang tersebut : ");
+        System.out.print("Masukkan harga jual barang tersebut : ");
         harga_jual = key.nextDouble();
 
         // menampilkan yang disimpan variable
-        System.out.println("------------------------------------");
-        System.out.println("Nama barang : " + nama_barang);
+        System.out.println("\nStock Gudang Rincian Barang");
+        System.out.println("###########################");
+        System.out.println("Nama barang   : " + nama_barang);
         System.out.println("Jumlah barang : " + jumlah_barang);
-        System.out.println("harga beli : " + String.format("%.02f", harga_beli));
-        System.out.println("harga jual : " + String.format("%.02f", harga_jual));
+        System.out.println("harga beli    : " + String.format("%.02f", harga_beli));
+        System.out.println("harga jual    : " + String.format("%.02f", harga_jual));
+
+        System.out.println("\n###### Bye " +nama_user+", see u again! ######");
     }
 
 }
